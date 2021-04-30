b44e9bbfc9e1b4d7510d41d7e08d29e8b0e702f7
omoluabidotcom <yyahaya222@yahoo.com>
2021-04-30 23:06:21 +0100

    Mission accomplished

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## This is a Simple 2-Display Calculator
 The idea was from a strangers github repository and I decided to replicate it here.
 A stranger am hoping will become a friend.
+
+Job Done.
+mission accomplished
--- index.php
+++ index.php
@@ -41,28 +41,28 @@ include 'inc/autoload.inc.php';
         ?>
      </fieldset>
         <!-- Below the button for the numbers and operators are added -->
-        <input type="button" value="7" class="numb" id="numb" onclick="form.numberSpace.value+='7'" />
+        <input type="button" value="7" class="numb" id="seven" onclick="form.numberSpace.value+='7'" />
         <input type="button" value="8" class="numb" id="numb" onclick="form.numberSpace.value+='8'" />
         <input type="button" value="9" class="numb" id="numb" onclick="form.numberSpace.value+='9'" />
-        <input type="button" value="/" class="numb" id="numb" onclick="form.numberSpace.value+='/'" /> 
+        <input type="button" value="/" class="numb" id="oper" onclick="form.numberSpace.value+='/'" /> 
         <br>
 
-        <input type="button" value="4" class="numb" id="numb" onclick="form.numberSpace.value+='4'" />
+        <input type="button" value="4" class="numb" id="four" onclick="form.numberSpace.value+='4'" />
         <input type="button" value="5" class="numb" id="numb" onclick="form.numberSpace.value+='5'" />
         <input type="button" value="6" class="numb" id="numb" onclick="form.numberSpace.value+='6'" />
-        <input type="button" value="*" class="numb" id="numb" onclick="form.numberSpace.value+='*'" /> 
+        <input type="button" value="*" class="numb" id="oper" onclick="form.numberSpace.value+='*'" /> 
         <br>
 
-        <input type="button" value="1" class="numb" id="numb" onclick="form.numberSpace.value+='1'" />
+        <input type="button" value="1" class="numb" id="one" onclick="form.numberSpace.value+='1'" />
         <input type="button" value="2" class="numb" id="numb" onclick="form.numberSpace.value+='2'" />
         <input type="button" value="3" class="numb" id="numb" onclick="form.numberSpace.value+='3'" />
-        <input type="button" value="-" class="numb" id="numb" onclick="form.numberSpace.value+='-'" /> 
+        <input type="button" value="-" class="numb" id="oper" onclick="form.numberSpace.value+='-'" /> 
         <br>
 
-        <button href="index.php" class="refresh"> C </button>
+        <button href="index.php" class="refresh" id="refresh" > C </button>
         <input type="button" value="0" class="numb" id="numb" onclick="form.numberSpace.value+='0'" />
-        <input type="button" value="." class="numb" id="numb" onclick="form.numberSpace.value+='.'" />
-        <input type="button" value="+" class="numb" id="numb" onclick="form.numberSpace.value='+'" /> 
+        <input type="button" value="." class="numb" id="oper" onclick="form.numberSpace.value+='.'" />
+        <input type="button" value="+" class="numb" id="oper" onclick="form.numberSpace.value='+'" /> 
         <br>
 
         <!-- Below the submit button is added -->
